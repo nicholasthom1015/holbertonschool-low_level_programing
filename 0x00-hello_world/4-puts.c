11721314849d79c28ef09e897a66ac09ba18e5e2
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-15 12:03:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ * Return: 0 if intended outcome. Error otherwise
+ */
 int main(void)
 {
 
 	char B[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(B);
-
-	return (0);
+}
+/**
+ * sub - Check if a number is greater than 0
+ * @a: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int sub(int a)
+{
+	return (a > 0);
 }
