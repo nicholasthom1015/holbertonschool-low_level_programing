261d10624c432ac9723acf47e660ba553cdffe0c
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 12:08:28 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <unisted.h>
+int main(void);
+/** prototype file for last project */
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+#endif
