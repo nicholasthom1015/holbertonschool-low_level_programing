03c12cdd09365d82e745bf06357578a10cdfab55
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 08:57:24 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the full alphabet with putchar
+ * Return: 0 if intented outcome. Error otherwise
+ */
+int main(void)
+{
+	char ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+	{		putchar(ch);
+	}
+		for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
