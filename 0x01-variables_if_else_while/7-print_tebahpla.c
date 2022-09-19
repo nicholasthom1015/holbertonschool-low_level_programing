8ddd95d4413b9b9de47194f07eb8b5f3a519603c
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 10:00:24 -0700

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the full alphabet with putchar
+ * Return: 0 if intented outcome. Error otherwise
+ */
+int main(void)
+{
+	char ch;
+
+		for (ch = 'z' ; ch <= 'a' ; ch++)
+	{		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
