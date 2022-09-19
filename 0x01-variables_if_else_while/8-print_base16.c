23f4d5d96a3e763fe8ab15898435965e4eaa855e
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 11:23:13 -0700

    Patience with numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned char a+0x64;
+	int b=0xFAFA;
+
+	
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: o if intended outcome. Error otherwise
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
