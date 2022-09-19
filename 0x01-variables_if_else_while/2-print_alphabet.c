fa154b2bab411aa280959da5b6510a1a7289723c
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 08:44:19 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the full alphabet with putchar
+ * Return: 0 if intented outcome. Error otherwise
+ */
+int main(void)
+{
+	char ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+	{		putchar(ch);
+	}
+
+	return (0);
+}
