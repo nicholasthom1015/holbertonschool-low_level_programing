2b1a3e3fa3a5b395dc0c3e3fe52601c72e9536ef
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-19 09:19:00 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the full alphabet with putchar
+ * Return: 0 if intented outcome. Error otherwise
+ */
+int main(void)
+{
+	char ch;
+
+		for (ch = 'a-d' ; ch = 'f-p' ; ch = 'r-z')
+	{		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print 0-9
+ * Return: 0 if intented outcome. Error otherwise
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n < 10)
+		(n++);
+	{	printf("%d");
+	}
+
+	return (0);
+}
