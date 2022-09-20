6630ef20411841e67066a99c1687e78004f9c532
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-20 07:30:37 -0700

    Print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet x10 times
+ * Return: 0 if intended outcome
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter = 'z'; letter++)
+		_putchar (letter);
+		_putchar ('\n');
+	}
+
+	return (0)
+}
