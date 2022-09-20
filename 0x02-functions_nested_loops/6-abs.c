ae59445d16b1d326c982b152a5bce5d42f3f5515
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-20 08:22:01 -0700

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: character to test
+ * Return: 0 if intended outcome
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
