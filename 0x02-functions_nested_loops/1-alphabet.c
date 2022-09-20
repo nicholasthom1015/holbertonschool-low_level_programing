a7514e63a236b21e5cd5576278dbd25602805f46
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-20 07:21:38 -0700

    Print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the full alphabet with putchar
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+	{	_putchar(ch);
+	}
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-#include <unisted.h>
-int main(void);
 /** prototype file for last project */
-int _putchar(char c);
+int _putchar(int c);
 void print_alphabet(void);
 int _islower(int c);
 #endif
