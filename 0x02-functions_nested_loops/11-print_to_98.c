feba8fa4270942717437ae31bbed92f2bb48b539
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-20 09:26:59 -0700

    To 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints to 98
+ * @n: character to test
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
