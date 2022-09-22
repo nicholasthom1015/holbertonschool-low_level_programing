7b5577abde178fda6b30d30cd97e2862d8acfb41
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 09:26:47 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,10 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
-			_putchar((num / 10) + '0');
-		_putchar (num + '0');
+				if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar (num + '0');
 		}	
-			
-	_putchar ('\n');
+		_putchar ('\n');
 	}
 }
