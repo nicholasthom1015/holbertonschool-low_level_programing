a0214807ab67d21aca5d6399856ff4a868b4b436
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 08:09:12 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * mul - multiply numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 /** prototype file for last project */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
