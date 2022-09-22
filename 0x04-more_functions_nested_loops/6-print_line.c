d47019df4627ecc7b9f29211199ff9024ab5e716
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 09:50:15 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * @n: test character
+ */
+void print_line(int n)
+{
+	int length;
+
+	if (n > 0)
+	{
+		for (length > 0; length < n; length++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
