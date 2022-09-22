6fe035b55c44212d9113a1006e34dc9436249d8e
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 12:18:31 -0700

    print_spuare

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,15 @@ void print_square(int size)
 	if (size > 0)
 	{
 		for (height = 0; height < size; height++)
-			{
-				for (length = 0; length < size; length++)
-					_putchar('#');
+		{
+			for (length = 0; length < size; length++)
+				_putchar('#');
 
-				if (height == size - 1)
-					continue;
-				_putchar('\n');
-			}
+			if (height == size - 1)
+				continue;
+			_putchar('\n');
+		}
 	}
+
+	_putchar('\n');
 }
