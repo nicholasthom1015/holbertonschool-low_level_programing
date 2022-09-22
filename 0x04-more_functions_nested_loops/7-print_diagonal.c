a674699f3926c5efac2ae6cf7a5e03fd9264bb35
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 11:10:36 -0700

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,13 +5,19 @@
  */
 void print_diagonal(int n)
 {
-	int length;
+	int length, space;
 
 	if (n > 0)
 	{
 		for (length = 0; length < n; length++)
+		{
+			for (space = 0; space < length; space++)
 			_putchar(' ');
 		_putchar('\\');
+
+		if (length == n - 1)
+			continue;
+		}
 	}
 
 	_putchar('\n');
