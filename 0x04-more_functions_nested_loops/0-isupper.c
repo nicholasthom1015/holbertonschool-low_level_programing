5dc142a5a3937923982c1d915d5dcc0a2110ec80
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-22 07:42:23 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to test
+ * Return: 1 if intended outcome
+ */
+int _isupper(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+/** prototype file for last project */
+int _isupper(int c);
+#endif
