d759d4cac309dc199c6aaf19f36e4793773b561d
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-23 08:50:47 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - length of string
+ * @s: test character
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 /** prototype file for last project */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
