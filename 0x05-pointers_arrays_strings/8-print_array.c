df2ae7c5a176ce08b1a3f530f46c47cde9349c0a
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-26 09:19:04 -0700

    print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array
+ * @a: test character
+ * @n: test character
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+
+	}
+}
