38fcdaefa3ff10d9beaa35b0d004dba2f1972bca
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-26 08:24:34 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,12 @@
 void rev_string(char *s)
 {
 	int len = 0, index = 0;
-	char tmp;
+	char tmp[999];
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= len / 2; index--)
+	while (s[index] != '\0')
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len = index - 1] = tmp;
+		tmp[index] = s[index];
+		index++;
+		len++;
 	}
 }
