d2fded172e6bbf0a7370273632394135e307735c
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-26 08:03:53 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: test character
+ */
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[len - index - 1];
+		s[len = index - 1] = temp;
+	}
+}
