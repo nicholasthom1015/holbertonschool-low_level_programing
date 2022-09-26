3614451b74891c90598d06c668663f644cd36141
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-26 07:46:20 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,17 @@
 #include "main.h"
-
+/**
+ * print_rev - print string in reverse
+ * @s: test character
+ */
 void print_rev(char *s)
 {
+	int length = 0, index;
 
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
