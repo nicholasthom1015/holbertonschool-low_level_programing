f8a7a4fefef77050b77a9dd09fecf0e8f8b2f2e6
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-09-29 09:11:35 -0700

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ * @a: test character
+ * @n; test character
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
