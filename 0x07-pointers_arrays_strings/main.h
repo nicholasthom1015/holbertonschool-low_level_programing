c5263aaa5a9d94137d7b1b4853206c48a6fa995a
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-03 09:27:50 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle0;
+char *_strstr(char *haystack, char *needle0);
+
 #endif
