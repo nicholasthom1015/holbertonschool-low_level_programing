87411ec693ad373e56c09c157fc9605e7f17ccd3
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-03 09:08:33 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - Get length of prefix substring
+ * @s: test caracter
+ * @accept: test caracter
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@
 char *_memset(char *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
