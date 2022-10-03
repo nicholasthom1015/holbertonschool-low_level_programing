44aa41cc0cacdf30a20bc755185d6952d53088fb
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-03 08:49:20 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,14 +3,14 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: test character
- * @b: test character
+ * @c: test character
  * @n: test character
  * Return: 0
  */
-void *_memset(char *s, int b, size_t n)
+void *_memset(char *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = b;
+	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <stddef.h>
-void *_memset(char *s, int b, size_t n);
+
+void *_memset(char *s, int c, size_t n);
 #endif
