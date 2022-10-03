d37f7e0c2927ef2f33c848da54681ab0e0d5f138
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-03 09:13:11 -0700

    strbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ char *_memset(char *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strbrk(char *s, char *accept);
 #endif
