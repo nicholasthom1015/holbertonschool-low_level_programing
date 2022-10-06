f893aa15936da8c4b4137be44c7641e70e63e2e6
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-06 09:02:28 -0700

    args.2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	int counter;
 
 	for (counter = 0; counter < argc; counter++)
-		printf("argv[%2d]; %s\n", counter, argv[counter]);
+		printf("argv[%2d]: %s\n", counter, argv[counter]);
 
 	return (0);
 }
