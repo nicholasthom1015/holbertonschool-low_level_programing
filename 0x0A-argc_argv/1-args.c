87a96d4722aec3c7a2048af12106b4b00a58f3fc
nicholas <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-10-06 08:53:30 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints number of arguements in program
+ * @argc: test character
+ * @argv: test character
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
