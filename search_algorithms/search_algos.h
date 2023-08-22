f6032eacefceffe6ed98263088d21f797756e200
nicholasthom1015 <103206977+nicholasthom1015@users.noreply.github.com>
2023-08-22 18:19:55 -0500

    Create search_algos.h

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCHALGO
+#define SEARCHALGO
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+void print_array(int *array, size_t size);
+
+#endif
